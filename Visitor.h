074ff7b91630da0626d66a76dd53a61e04e2af58
Mostafa Ayman Shalapy <mooostafa05@gmail.com>
2025-11-12 15:21:47 +0200

    Update Visitor.h

--- Visitor.h
+++ Visitor.h
@@ -1,15 +1,19 @@
 #ifndef VISITOR_H
 #define VISITOR_H
-
+#include <iostream>
 #include <string>
 using namespace std;
 
-//define visitor class here
-
-
-
-
-
-
+class Visitor{
+    private:
+    string visitorName;
+    int ticketsBought;
+    
+    public:
+    Visitor();
+    Visitor(string v, int t);
+    ~Visitor();
+    void displayInfo();
+};
 
 #endif
