196a19060af0db2852d7f42c7e6f08aa61dd33d6
Mostafa Ayman Shalapy <mooostafa05@gmail.com>
2025-11-12 14:56:19 +0200

    Define Animal class with basic attributes and methods
    Added Animal class with attributes and methods.

--- Animal.h
+++ Animal.h
@@ -1,11 +1,25 @@
 #ifndef ANIMAL_H
 #define ANIMAL_H
-
+#include <iostream>
 #include <string>
 using namespace std;
 
-//define animal class here 
+class Animal{
+    private:
+    string name;
+    int age;
+    bool isHungry;
+    
+    public:
+    Animal();
+    Animal(string n, int a, bool i);
+    ~Animal();
+    string getName();
+    void setName(string n);
+    int getAge();
+    void setAge(int a);
+    void display();
+    void feed();
+};
 
-
-
-#endif
+#endif 
