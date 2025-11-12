c702c4fc587a4c9340e2887f72294a915490d2b6
Mostafa Ayman Shalapy <mooostafa05@gmail.com>
2025-11-12 15:09:13 +0200

    Enhance Reptile class with additional includes
    Added includes for iostream and string, and updated the Reptile class definition.

--- Reptile.h
+++ Reptile.h
@@ -1,16 +1,20 @@
+#include "Animal.h"
 #ifndef REPTILE_H
 #define REPTILE_H
+#include <iostream>
+#include <string>
+using namespace std;
 
-#include "Animal.h"
-
-//define reptile class here
-
-
-
-
-
-
-
-
+class Reptile : public Animal{
+    private:
+    bool isVenomous;
+    
+    public: 
+    Reptile();
+    Reptile(string n, int a, bool i, bool v);
+    ~Reptile();
+    bool getIsVenomous();
+    void setIsVenomous();
+};
 
 #endif
